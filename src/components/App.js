a900135e83b16ae4db62375ebf5b808b17b148d1
Jose Daniel Hernandez <josedanielhq@gmail.com>
2020-07-23 13:53:20 -0500

    Listo

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Menu from './Menu';
 import Usuarios from './Usuarios';
 import Publicaciones from './Publicaciones';
-
-const Tareas = () => <div>Tareas</div>;
+import Tareas from './Tareas';
+import TareasGuardar from './Tareas/Guardar';
 
 const App = () => (
   <BrowserRouter>
@@ -14,6 +14,13 @@ const App = () => (
       <Route exact path="/" component={Usuarios} />
       <Route exact path="/tareas" component={Tareas} />
       <Route exact path="/publicaciones/:key" component={Publicaciones} />
+      <Route exact path="/tareas/guardar" component={TareasGuardar} />
+      <Route
+        exact
+        path="/tareas/guardar/:usu_id/_tar_id"
+        component={TareasGuardar}
+      />
+
       {/* el :key es para recibir los parametros por URL que le pasamos en el componente Tabla*/}
     </div>
   </BrowserRouter>
